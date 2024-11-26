3753712976cc9771bbf9b21195042b6115efb130
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-11-25 23:15:35 -0300

    Primeiras configurações do IJ

---
+++ Main.java
@@ -0,0 +1,31 @@
+/*
+ * bytes => 1 byte (-128 a 127)
+ * short => 2 bytes
+ * int => 4 bits
+ * long => 8 bits
+ *
+ *
+ * float
+ * double
+ *
+ * String (classe, não tipo primitivo)
+ * char
+ *
+ * boolean
+ *
+ *
+ * var numero = 10; // se usar var ela faz a tipagem automática da variável
+ *
+ *
+ * */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
