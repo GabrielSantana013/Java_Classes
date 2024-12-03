e98183f24df0f3ac304aa32be846f5afbc13e4c7
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-12-03 00:41:40 -0300

    feat: Aula Construtores e Sobrecarga

--- application/Program.java
+++ application/Program.java
@@ -1,27 +1,37 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
-
-import static util.Calculator.*;
+import entities.Product;
 
 public class Program {
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter radius:");
-        double radius = sc.nextDouble();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
 
-        double c = Circumference(radius);
-        double v = Volume(radius);
+        Product product = new Product(name,price);
+        System.out.println("Product data: " + product);
+        System.out.println();
 
-        System.out.printf("Circunference: %.2f\n", c);
-        System.out.printf("Volume: %.2f\n", v);
-        System.out.printf("PI value %.4f", PI);
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
 
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
         sc.close();
     }
-
-
 }
