efc3f8997c1e2c2a5250a3d808d8b5124acb5c04
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-12-12 00:48:16 -0300

    feat: adicionado aulas sobre cálculo entre DtHr

--- application/ProgramInstanciacaoDtEHora.java
+++ application/ProgramInstanciacaoDtEHora.java
@@ -1,15 +1,14 @@
 package application;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class ProgramInstanciacaoDtEHora {
 
     public static void main(String[] args)
     {
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        /*DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         LocalDate d01 = LocalDate.now();
@@ -27,7 +26,11 @@ public class ProgramInstanciacaoDtEHora {
         LocalDate d10 = LocalDate.of(2022, 7, 20);
         LocalDateTime d11 = LocalDateTime.of(2022,7,20,1,30);
 
-        System.out.println(d01);
+        LocalDate r1 = LocalDate.ofInstant(d07, ZoneId.systemDefault());*/
+
+
+
+        /*System.out.println(d01);
         System.out.println(d02);
         System.out.println(d03);
         System.out.println(d04);
@@ -38,6 +41,36 @@ public class ProgramInstanciacaoDtEHora {
         System.out.println(d09);
         System.out.println(d10);
         System.out.println(d11);
+        System.out.println(r1);*/
+
+        LocalDate d01 = LocalDate.parse("2024-12-12");
+        LocalDateTime d02 = LocalDateTime.parse("2024-12-12T01:30:26");
+        Instant d03 = Instant.parse("2024-12-12T01:30:26Z");
+
+        LocalDate pastWeekLocalDate = d01.minusDays(7);
+        LocalDate nextWeekLocalDate = d01.plusDays(7);
+
+        Instant pastWeekInstant = d03.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d03.plus(7, ChronoUnit.DAYS);
+
+        LocalDateTime pastWeekLocalDateTime = d02.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d02.plusDays(7);
+
+        System.out.println(pastWeekLocalDate);
+        System.out.println(nextWeekLocalDate);
+        System.out.println(pastWeekInstant);
+        System.out.println(nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDateTime, nextWeekLocalDateTime);
+        Duration t2 = Duration.between(pastWeekLocalDate.atStartOfDay(), d01.atStartOfDay());
+        Duration t3 = Duration.between(pastWeekInstant, d03);
+
+
+        System.out.println(t1.toDays());
+        System.out.println(t2.toDays());
+        System.out.println(t3.toDays());
+
+
     }
 
 }
